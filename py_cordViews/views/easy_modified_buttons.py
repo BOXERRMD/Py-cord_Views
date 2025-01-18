0e885b1ae67453ed520d4152cd00eb9b642e1c04
BOXER <vagabonwalybi@gmail.com>
2025-01-18 21:13:36 +0100

    [UPDATE] Easy_modified_buttons
    Finalisation de la class

--- py_cordViews/views/easy_modified_buttons.py
+++ py_cordViews/views/easy_modified_buttons.py
@@ -1,9 +1,9 @@
-from discord import Interaction
+from discord import Interaction, ApplicationContext, Message
 from discord.ui import View, Item
 from typing import Union, Callable, Coroutine
 
 from py_cordViews.typeViews import T_views
-from .errors import CustomIdRequired, CustomIDNotFound
+from .errors import CustomIDNotFound
 
 
 
@@ -22,19 +22,54 @@ class EasyModifiedViews(View):
         super().__init__(*items, timeout=timeout)
         self.__timeout: Union[float, None] = timeout
         self.__disabled_on_timeout: bool = disabled_on_timeout
-        self.__callback: dict[str, Callable[[Interaction], None]] = {}
+        self.__callback: dict[str, dict[str, Union[Callable[[Interaction], None], T_views]]] = {}
+        self.__ctx: Union[Message, Interaction] = None
 
-    def add_views(self,
-                   *views: T_views) -> None:
+    def __check_custom_id(self, custom_id: str) -> None:
+        """
+        Check if the custom_id is alive
+        :param custom_id: ID to find
+        :raise: CustomIDNotFound
+        """
+        if custom_id not in self.__callback.keys():
+            raise CustomIDNotFound()
+
+    async def respond(self, ctx: ApplicationContext, *args, **kwargs):
+        """
+        Respond at the ApplicationContext
+        """
+        self.__ctx = await ctx.respond(*args, **kwargs)
+
+    def add_items(self,
+                   *items: T_views) -> None:
         """
         Add all items in the View.
         custom_id REQUIRED !
         """
 
-        for ui in views:
+        for ui in items:
 
-            self.__callback[ui.custom_id] = None
+            self.__callback[ui.custom_id] = {'ui': ui, 'func': None}
             self.add_item(ui)
+
+    async def update_items(self, *items: T_views):
+        """
+        Update all views.
+        Append items if custom_ids not in the view
+        Update items if custom_ids in the view
+        :param items: items to update
+        """
+
+        for item in items:
+
+            try:
+                self.__check_custom_id(item.custom_id)
+                self.__callback[item.custom_id]['ui'] = item
+
+            except CustomIDNotFound:
+                self.add_items(item)
+
+        await self.__update()
 
     def set_callable_decorator(self, custom_id: str):
         """
@@ -56,10 +91,9 @@ class EasyModifiedViews(View):
         """
 
         def decorator(coroutine: Coroutine):
-            if custom_id not in self.__callback.keys():
-                raise CustomIDNotFound
+            self.__check_custom_id(custom_id)
 
-            self.__callback[custom_id] = coroutine
+            self.__callback[custom_id]['func'] = coroutine
             return coroutine
 
         return decorator
@@ -81,17 +115,16 @@ class EasyModifiedViews(View):
         view.set_callable(custom_id='test_ID', coroutine=rep)
         await ctx.respond('coucou', view=view)
         """
-        if custom_id not in self.__callback.keys():
-            raise CustomIDNotFound
+        self.__check_custom_id(custom_id)
 
-        self.__callback[custom_id] = coroutine
-
+        self.__callback[custom_id]['func'] = coroutine
 
     async def interaction_check(self, interaction: Interaction) -> bool:
         """
         Func to apply items
         """
-        await self.__callback[interaction.custom_id](interaction)
+        print(self.__callback[interaction.custom_id])
+        await self.__callback[interaction.custom_id]['func'](interaction)
 
         return True
 
@@ -110,6 +143,24 @@ class EasyModifiedViews(View):
 
         self.disable_all_items(exclusions=[self.get_item(id_) for id_ in self.__callback.keys() if id_ not in custom_ids])
 
+    async def delete_items(self, clear_all: bool = False, *custom_ids: str):
+        """
+        Delete an item on the view
+        :param custom_ids: IDs of items to delete
+        :param clear_all: Clear all items in the view
+        :raise: CustomIDNotFound
+        """
+        if clear_all:
+            self.clear_items()
+
+        else:
+
+            for custom_id in custom_ids:
+                self.__check_custom_id(custom_id)
+                self.remove_item(self.get_item(custom_id))
+
+        await self.__update()
+
     def on_timeout(self) -> None:
         """
         Called if timeout view is finished
@@ -117,11 +168,27 @@ class EasyModifiedViews(View):
         if self.__disabled_on_timeout:
             self.shutdown()
 
-
     async def __update(self):
+        """
+        Update the View on the attached message.
+        """
+        if self.message:
+            await self.message.edit(view=self)
 
-        print(self.message)
-        await self.message.edit(view=self)
+        else:
+            await self.__ctx.edit(view=self)
 
+    @property
+    def get_uis(self) -> list[T_views]:
+        """
+        Get all uis in the view
+        """
+        return [i['ui'] for i in self.__callback.values()]
 
-
+    def get_ui(self, custom_id: str) -> T_views:
+        """
+        Get an ui in the view
+        :raise: CustomIDNotFound
+        """
+        self.__check_custom_id(custom_id)
+        return self.__callback[custom_id]['ui']
--- py_cordViews/views/errors.py
+++ py_cordViews/views/errors.py
@@ -4,10 +4,6 @@ class Py_cordViews(Exception):
     """
 
 
-class CustomIdRequired(Py_cordViews):
-    def __init__(self):
-        super().__init__(f"custom_id is required in a UI !")
-
 class CustomIDNotFound(Py_cordViews):
     def __init__(self):
         super().__init__(f"custom_id not found !")
